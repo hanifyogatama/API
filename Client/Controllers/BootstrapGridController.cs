bf7657164952a4c288b8bfd6b671e78dd0180962
Hanif yogatama <hanifyotama@gmail.com>
2022-02-04 09:37:21 +0700

    add task grid system bootsrap

---
+++ Client/Controllers/BootstrapGridController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Client.Controllers
+{
+    public class BootstrapGridController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Task()
+        {
+            return View();
+        }
+    }
+}
